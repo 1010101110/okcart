b1fbe2c4e9efbdbe3dfe2c9a338464f1f7d0f1ed
Jonathan Hiatt <jonathan.w.hiatt@us.abb.com>
2017-12-14 09:50:51 -0600

    separate config files

---
+++ config-s.js
@@ -0,0 +1,14 @@
+//here you can put secret stuff only the server knows
+
+export default server = {    
+    //stripe secret api key
+    stripesk:"sk_test_AbLFTbzSIGCoNxJwixfU8OgG",
+
+    //email used for contact form and order updates
+    storeEmailService:"Gmail",
+    storeEmailUser:"asdf@gmail.com",
+    storeEmailPass:"asdf123",
+
+    //password hash to /admin backend
+    adminPassHash: "$2a$10$1hFcepwZlTmpmrM.QTvZtuczVJswgTUG8pn9nlidS39rEJ/aK7U2."
+}
